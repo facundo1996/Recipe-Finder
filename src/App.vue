9711c654accc82369d9961fd4a23893c1a3a5e0e
facundo1996 <ffaacu1@gmail.com>
2021-01-20 17:12:52 -0300

    Structure created, created component (CreateRecipe.vue).

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div class="container">
+    <h1>Recipe Finder</h1>
+    <CreateRecipe/>
   </div>
 </template>
 
 <script>
+import CreateRecipe from './components/CreateRecipe'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components:{
+    CreateRecipe
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+h1{
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/CreateRecipe.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+        <h2>Create new recipe</h2>
+        <form>
+          <!-- Recipe Name -->
+          <div>
+            <input  v-if="seen_input_name" type="text" placeholder="Name" id="input_name">
+            <button v-if="visible_button" v-on:click="create_recipe">Create recipe</button>
+            <p v-if="seen_p">{{recipe[0]}}</p>
+          </div>
+
+          <!-- Ingredients -->
+          <div>
+            <h3>Ingredients</h3>
+            <input type="text" placeholder="Ingredient">
+            <button type="submit">Add ingredient</button>
+          </div>
+
+          <!-- Sweet or salty -->
+          <div>
+              <div>
+                <input type="radio" name="radio-group" id="Sweet" value="Sweet">
+                <label for="js" class="radio-label">Sweet</label>
+              </div>
+              <div>
+                <input type="radio" name="radio-group" id="Salty" value="Salty">
+                <label for="tricky">Salty</label>
+              </div>
+          </div>
+
+          <button type="submit">Add Recipe</button>
+
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CreateRecipe',
+  data () {
+    return{
+      seen_input_name: true,
+      visible_button: true,
+      seen_p: false,
+      recipe: [],
+      recipes: [],
+    }
+  },
+  methods: {
+    create_recipe(){
+      
+      if(input_name.value === ""){
+        input_name.style.background="pink"
+      }else{
+        this.seen_input_name = false
+        this.visible_button = false
+        this.seen_p = true
+        this.recipe.push(input_name.value)
+        console.log(this.recipe)
+      }
+      
+    }
+
+  }
+}
+
+</script>
+
+<style>
+
+</style>
